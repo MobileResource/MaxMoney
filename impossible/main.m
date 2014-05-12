d0ce45ce81943c043b1c2a733d776f43b1ab5ed4
RSI <victory@RSIs-Mac.local>
2014-05-12 12:26:14 +0800

    Initial Commit

---
+++ impossible/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  impossible
+//
+//  Created by RSI on 5/12/14.
+//  Copyright game 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
